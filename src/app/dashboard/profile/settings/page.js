4fd82cafbf1e7cf957a1e2c427451fef6ef125cf
munir <smsirajulmonir@gmail.com>
2025-04-09 11:00:00 +0600

    routing system recaped

---
+++ src/app/dashboard/profile/settings/page.js
@@ -0,0 +1,10 @@
+
+const ProfileSettingPage = () => {
+    return (
+        <div>
+            <h1 className='text-3xl'>Profile Setting Page</h1>
+        </div>
+    );
+};
+
+export default ProfileSettingPage;
